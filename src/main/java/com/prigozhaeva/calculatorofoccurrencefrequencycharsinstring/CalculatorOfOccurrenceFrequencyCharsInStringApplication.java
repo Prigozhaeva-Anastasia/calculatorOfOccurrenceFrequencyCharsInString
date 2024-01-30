2500a15f75a83c294bc41025e47268ac9cdcfa13
Prigozhaeva-Anastasia <nastaprigozaeva20@gmail.com>
2024-01-30 11:01:26 +0300

    Initial commit

---
+++ src/main/java/com/prigozhaeva/calculatorofoccurrencefrequencycharsinstring/CalculatorOfOccurrenceFrequencyCharsInStringApplication.java
@@ -0,0 +1,13 @@
+package com.prigozhaeva.calculatorofoccurrencefrequencycharsinstring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorOfOccurrenceFrequencyCharsInStringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorOfOccurrenceFrequencyCharsInStringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prigozhaeva/calculatorofoccurrencefrequencycharsinstring/CalculatorOfOccurrenceFrequencyCharsInStringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prigozhaeva.calculatorofoccurrencefrequencycharsinstring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorOfOccurrenceFrequencyCharsInStringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
